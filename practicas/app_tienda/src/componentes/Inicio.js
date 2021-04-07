2632ef2c7853846457e6f1101d704943a9423178
Cesardlr <cesardelriodlr@outlook.com>
2021-04-07 13:23:33 -0600

    creacion del archivo y del menu

---
+++ practicas/app_tienda/src/componentes/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Inicio = () => {
+    return ( 
+        <div>
+            <h1>Inicio</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat voluptates dicta ducimus omnis officiis neque in? Nisi nostrum, blanditiis cum doloremque est exercitationem assumenda, soluta dolor voluptate maxime saepe voluptas.</p>
+        </div>
+     );
+}
+ 
+export default Inicio;
---
+++ practicas/app_tienda/src/componentes/Tienda.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Tienda = () => {
+    return ( 
+        <div>
+            <h1>Tienda</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat voluptates dicta ducimus omnis officiis neque in? Nisi nostrum, blanditiis cum doloremque est exercitationem assumenda, soluta dolor voluptate maxime saepe voluptas.</p>
+        </div>
+     );
+}
+ 
+export default Tienda;
