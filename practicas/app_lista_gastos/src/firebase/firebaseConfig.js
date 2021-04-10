b37ee36d4d24a6a79a43825b565cff5c3f7f6af2
Cesardlr <cesardelriodlr@outlook.com>
2021-04-10 15:46:55 -0600

    configurando firebase

---
+++ practicas/app_lista_gastos/src/firebase/firebaseConfig.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+
+// Este es el que sirve para poder autenticar
+import 'firebase/auth'
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export {db, auth}
