cba58ee4d6e04d58571bce28f02b050337b5e8ac
Cesardlr <cesardelriodlr@outlook.com>
2021-04-10 02:58:41 -0600

    terminando de configurar el archiv

--- practicas/app_lista_gastos/src/index.js
+++ practicas/app_lista_gastos/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import WebFont from 'webfontloader'
+import Contenedor from './elementos/Contenedor'
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import InicioSesion from './componentes/InicioSesion'
+import EditarGasto from './componentes/EditarGasto'
+import GastosPorCategoria from './componentes/GastosPorCategoria'
+import RegistroUsuarios from './componentes/RegistroUsuarios'
+import ListaDeGastos from './componentes/ListaDeGastos'
 
 WebFont.load({
 
@@ -15,7 +22,18 @@ WebFont.load({
 
 const Index = () => {
   return(
-    <App />
+    <BrowserRouter>
+      <Contenedor>
+        <Switch>
+          <Route path="/iniciar-sesion" component={InicioSesion}/>
+          <Route path="/crear-cuenta" component={RegistroUsuarios}/>
+          <Route path="/categorias" component={GastosPorCategoria}/>
+          <Route path="/lista" component={ListaDeGastos}/>
+          <Route path="/editar/:id" component={EditarGasto}/>
+          <Route path="/" component={App}/>
+        </Switch>
+      </Contenedor>
+    </BrowserRouter>
   );
 };
 
