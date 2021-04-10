59daf9fd41540b7226ad941f6b220d9ae0e568b8
Cesardlr <cesardelriodlr@outlook.com>
2021-04-10 15:31:59 -0600

    creando la pagina de crear cuenta e inicio sesion

--- practicas/app_lista_gastos/src/componentes/InicioSesion.js
+++ practicas/app_lista_gastos/src/componentes/InicioSesion.js
@@ -1,8 +1,55 @@
 import React from 'react'
+import {Helmet} from 'react-helmet'
+import {Header, Titulo, ContenedorHeader} from './../elementos/Header'
+import Boton from './../elementos/Boton'
+import {Formulario,Input,ContenedorBoton} from './../elementos/ElementosDeFormulario'
+
+// Asi importamos una imagen svg
+import {ReactComponent as SvgLogin} from './../imagenes/login.svg'
+import styled from 'styled-components';
+
+const Svg = styled(SvgLogin)`
+    width:100%;
+    max-height:12.5rem; /* 200px */
+    margin-bottom:1.25rem; /* 20px */
+`
 
 const InicioSesion = () => {
     return (
-        <h1>Inicio sesion</h1>
+        <>
+            <Helmet>
+                <title>Iniciar Sesion</title>
+            </Helmet>
+
+            <Header>
+                <ContenedorHeader>
+                    <Titulo>Iniciar Sesion</Titulo>
+
+                    <div>
+                        <Boton to="/crear-cuenta">Registrarse</Boton>
+                    </div>
+                </ContenedorHeader>
+            </Header>
+
+            <Formulario>
+                <Svg>{SvgLogin}</Svg>
+                <Input 
+                    type="email"
+                    name="email"
+                    placeholder="Correo Electronico"
+                />
+
+                <Input 
+                    type="password"
+                    name="passwrod"
+                    placeholder="Contraseña"
+                />
+
+                <ContenedorBoton>
+                <Boton as="button" primario type="submit">Iniciar Sesion</Boton>
+                </ContenedorBoton>
+            </Formulario>
+    </>
     );
 }
 
--- practicas/app_lista_gastos/src/componentes/RegistroUsuarios.js
+++ practicas/app_lista_gastos/src/componentes/RegistroUsuarios.js
@@ -1,8 +1,62 @@
 import React from 'react'
+import {Helmet} from 'react-helmet'
+import {Header, Titulo, ContenedorHeader} from './../elementos/Header'
+import Boton from './../elementos/Boton'
+import {Formulario,Input,ContenedorBoton} from './../elementos/ElementosDeFormulario'
+
+// Asi importamos una imagen svg
+import {ReactComponent as SvgLogin} from './../imagenes/registro.svg'
+import styled from 'styled-components';
+
+const Svg = styled(SvgLogin)`
+    width:100%;
+    max-height:6.25rem; /* 100px */
+    margin-bottom:1.25rem; /* 20px */
+`
+
 
 const RegistroUsuarios = () => {
     return (
-        <h1>RegistorUsuarios</h1>
+        <>
+            <Helmet>
+                <title>Crear cuenta</title>
+            </Helmet>
+
+            <Header>
+                <ContenedorHeader>
+                    <Titulo>Crear Cuenta</Titulo>
+
+                    <div>
+                        <Boton to="/iniciar-sesion">Iniciar Sesion</Boton>
+                    </div>
+                </ContenedorHeader>
+            </Header>
+
+            <Formulario>
+                <Svg>{SvgLogin}</Svg>
+                <Input 
+                    type="email"
+                    name="email"
+                    placeholder="Correo Electronico"
+                />
+
+                <Input 
+                    type="password"
+                    name="passwrod"
+                    placeholder="Contraseña"
+                />
+
+                <Input 
+                    type="password"
+                    name="passwrod2"
+                    placeholder="Verifique la contraseña"
+                />
+                
+                <ContenedorBoton>
+                <Boton as="button" primario type="submit">Crear Cuenta</Boton>
+                </ContenedorBoton>
+            </Formulario>
+        </>
     );
 }
 
