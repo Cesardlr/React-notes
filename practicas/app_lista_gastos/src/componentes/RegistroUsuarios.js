06f2721f6ee1bd7c4e720c6f01b93e92895df6d5
Cesardlr <cesardelriodlr@outlook.com>
2021-04-10 19:12:52 -0600

    validacion del formulario

--- practicas/app_lista_gastos/src/componentes/RegistroUsuarios.js
+++ practicas/app_lista_gastos/src/componentes/RegistroUsuarios.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Helmet} from 'react-helmet'
 import {Header, Titulo, ContenedorHeader} from './../elementos/Header'
 import Boton from './../elementos/Boton'
@@ -16,6 +16,56 @@ const Svg = styled(SvgLogin)`
 
 
 const RegistroUsuarios = () => {
+    const [correo, establecerCorreo] = useState('')
+    const [password, establecerPassword] = useState('')
+    const [password2, establecerPassword2] = useState('')
+
+    const handleChange = (e)=>{
+        // Aqui vamos a ver cual es el name del input y dependiendo de ese name, vamos  a ejecutar la respectiva funcion para cambiar el estado
+
+        switch (e.target.name) {
+            case 'email':
+                establecerCorreo()
+                break;
+            case 'password':
+                establecerPassword()
+                break;
+            case 'password2':
+                establecerPassword2()
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+
+        // Hacemos una pequeña validacion con expresiones regulares
+        const expresionRegular = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+
+        
+        // Aqui viendo en un condcional si se esta incumpliendo el test, osea que si correo no coincide con la expreisonRegular
+        if( !expresionRegular.test(correo)){
+            console.log('no es un correo valido')
+            return;
+        }
+
+        // Condicional ver si tiene ntexto
+        if(correo === '' || password === '' || password2 === ''){
+            console.log('por favor rellena los datos')
+            return
+        }
+
+        // Condicioinal contraseñas iguales
+        if(password !== password2){
+            console.log('las contraseñas si coinciden')
+            return;
+        }
+
+    }
+
     return (
         <>
             <Helmet>
@@ -32,24 +82,30 @@ const RegistroUsuarios = () => {
                 </ContenedorHeader>
             </Header>
 
-            <Formulario>
+            <Formulario onSubmit={handleSubmit}>
                 <Svg>{SvgLogin}</Svg>
                 <Input 
                     type="email"
                     name="email"
                     placeholder="Correo Electronico"
+                    value={correo}
+                    onChange={handleChange}
                 />
 
                 <Input 
                     type="password"
                     name="passwrod"
                     placeholder="Contraseña"
+                    value={password}
+                    onChange={handleChange}
                 />
 
                 <Input 
                     type="password"
                     name="passwrod2"
                     placeholder="Verifique la contraseña"
+                    value={password2}
+                    onChange={handleChange}
                 />
                 
                 <ContenedorBoton>
